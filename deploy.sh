eeba84165005b269febd2890984144057e6f58e5
Charles Feval <cfe84@users.noreply.github.com>
2017-04-07 14:44:08 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
+
 BRANCH=$1
+VERSION=`git describe --always`
 
 cd $DEPLOYMENT_FOLDER
 
 git add -A
-git commit -m "`date`"
+git commit -m "Commit $VERSION - Build as of `date`"
 git push origin $BRANCH
 
 cd -
